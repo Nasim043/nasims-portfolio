4a0ae87ae69d377f28a15e891d713f1c82763aff
Nasim <nasimcse16@gmail.com>
2023-06-17 19:25:39 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+				dark: '#10101A',
+				light: '#fcfcfc',
+				primary: '#8353E2',
+				secondary: '#05B19A',
+			},
+      fontFamily: {
+        mysignature: ["Great Vibes"],
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 }
